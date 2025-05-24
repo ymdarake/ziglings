a6f030f965d9d644b382acf9c93f4f2006acda55
ymdarake <sakamoto.takumim.dev@gmail.com>
2025-05-24 12:39:32 +0900

    :sparkles: 78, sentinel

--- exercises/078_sentinels3.zig
+++ exercises/078_sentinels3.zig
@@ -21,7 +21,16 @@ pub fn main() void {
     const data: [*]const u8 = "Weird Data!";
 
     // Please cast 'data' to 'printable':
-    const printable: [*:0]const u8 = ???;
+    const printable: [*:0]const u8 = @ptrCast(data);
+    // Now we can print it!
+    // Note: this is a little bit naughty, but it works!
+    // The @ptrCast() builtin is a little bit like a magic wand
+    // that lets you change the type of a pointer without
+    // changing the data it points to. It's a powerful tool, but
+    // you have to be careful with it. If you use it wrong, you
+    // can end up with a pointer that points to the wrong type of
+    // data, which can cause all sorts of problems. So use it
+    // wisely!
 
     print("{s}\n", .{printable});
 }
